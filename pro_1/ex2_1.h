cbeea3b42e2a4e100bc1573132e34de35fdb2f13
XueLianjie <Lianjie_Xue@sjtu.edu.cn>
2017-03-07 21:41:29 +0800

    add some files

---
+++ pro_1/ex2_1.h
@@ -0,0 +1,14 @@
+#ifndef ex2_1_H
+#define ex2_1_H
+
+#include <QtGui/QMainWindow>
+
+class ex2_1 : public QMainWindow
+{
+Q_OBJECT
+public:
+    ex2_1();
+    virtual ~ex2_1();
+};
+
+#endif // ex2_1_H
